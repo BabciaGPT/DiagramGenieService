af773fb9d7d9109b3389598b5cee76744e635888
matiz22 <mateuszmalich@gmail.com>
2025-03-23 11:33:05 +0100

    Message model

---
+++ rest/models/Message.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from rest.models.MessageType import MessageType
+
+
+class Message(BaseModel):
+    message: str
+    diagram_base64: Optional[str] = None
+    message_type: MessageType
+    timestamp: datetime = datetime.now()
