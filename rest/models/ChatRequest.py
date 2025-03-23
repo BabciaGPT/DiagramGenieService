26b0f8a54e5a5e2d407a161df459c743b7a6c988
matiz22 <mateuszmalich@gmail.com>
2025-03-23 11:31:41 +0100

    renaming property

--- rest/models/ChatRequest.py
+++ rest/models/ChatRequest.py
@@ -7,12 +7,12 @@ class ChatRequest(BaseModel):
     """
     Request model for the chatbot.
     - user_id: Unique identifier for the user.
-    - session_id: Optional identifier for a session if you want to separate multiple conversations.
+    - conversation_id: Optional identifier for a session if you want to separate multiple conversations.
     - message: The incoming message from the user.
     """
 
     user_id: str = Field(..., description="Unique identifier for the user")
     message: str = Field(..., description="Incoming message from the user")
-    session_id: Optional[str] = Field(
+    conversation_id: Optional[str] = Field(
         None, description="Optional session identifier for the conversation"
     )
