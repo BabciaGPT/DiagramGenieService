e8c76fbaed91b2cf25ddb969d9025e5c5f7d3121
matiz22 <mateuszmalich@gmail.com>
2025-03-31 09:35:59 +0200

    removing user id

--- rest/models/ChatRequest.py
+++ rest/models/ChatRequest.py
@@ -6,7 +6,6 @@ from pydantic import BaseModel, Field
 class ChatRequest(BaseModel):
     """
     Request model for the chatbot.
-    - user_id: Unique identifier for the user.
     - conversation_id: Optional identifier for a session if you want to separate multiple conversations.
     - message: The incoming message from the user.
     """
