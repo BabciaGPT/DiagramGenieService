6f8f7cc5acba317d7e43dde789b09f868e7c3772
matiz22 <mateuszmalich@gmail.com>
2025-03-31 18:46:46 +0200

    deleting print

--- rest/util/check_code_base64.py
+++ rest/util/check_code_base64.py
@@ -9,5 +9,4 @@ def check_code_base64(ai_response, generator):
             )
             return base64.b64encode(diagram_base64).decode("utf-8")
         except Exception as e:
-            print(str(e))
             return None
