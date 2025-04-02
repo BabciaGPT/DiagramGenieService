2c47753a59338e6f45274406ce6543eafc79392e
matiz22 <mateuszmalich@gmail.com>
2025-04-02 10:34:38 +0200

    cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from firebase.util.init import init_firebase
 from rest.routers.auth_router import auth_router
@@ -8,6 +9,14 @@ from rest.routers.chat_routes import chat_router
 from rest.routers.conversation_routes import conversation_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 load_dotenv()
 init_firebase()
 app.include_router(auth_router)
