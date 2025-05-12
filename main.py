f93ebea0342ab0e4d0d0660118ba422f9f3ba045
matiz22 <mateuszmalich@gmail.com>
2025-05-12 15:49:35 +0200

    add plantuml router to main application

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from firebase.util.init import init_firebase
 from rest.routers.auth_router import auth_router
 
 from rest.routers.chat_routes import chat_router
+from rest.routers.code_router import plantuml_router
 from rest.routers.conversation_routes import conversation_router
 from rest.routers.project_routes import project_router
 
@@ -38,3 +39,4 @@ app.include_router(auth_router)
 app.include_router(chat_router)
 app.include_router(conversation_router)
 app.include_router(project_router)
+app.include_router(plantuml_router)
